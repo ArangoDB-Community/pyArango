d7bcec1d121a19d1258c2efc6da2e700796528bd
Holger Müller <holgerm1969@gmx.de>
2016-08-21 16:15:01 +0200

    Fixed example graph
      - set package name as _key -> edges can point to documents
      - ignore duplicate packages on import
      - create graph definition
      - a little bit of lintify - code still ugly and non pythonic ;-)

--- examples/debiangraph.py
+++ examples/debiangraph.py
@@ -1,10 +1,22 @@
-#!python3
-# # example to build a file with all packages known to your system for a debian jessie:
-# for i in `ls /var/lib/apt/lists/*debian_dists_jessie_* |grep -v i386 |grep -v Release`; do cat $i >> /tmp/allpackages; echo >> /tmp/allpackages; done 
-# # (all debian based distros have a set of files in /var/lib/apt/lists; in doubt create a filter for your distro)
+#/usr/bin/env python3
+"""
+example to build a file with all packages known to your system for a debian jessie:
 
-# pip3 install pyArango
-# pip3 install deb_pkg_tools
+for i in $(ls /var/lib/apt/lists/*debian_dists_jessie_* |\
+           grep -v i386 |grep -v Release); do
+    cat $i >> /tmp/allpackages;
+    echo >> /tmp/allpackages;
+done
+
+All debian based distros have a set of files in /var/lib/apt/lists.
+In doubt create a filter for your distro
+
+Install needed Python modules:
+
+pip3 install pyArango
+pip3 install deb_pkg_tools
+"""
+
 import deb_pkg_tools
 from deb_pkg_tools.control import deb822_from_string
 from deb_pkg_tools.control import parse_control_fields
@@ -16,30 +28,48 @@ from pyArango.query import *
 from pyArango.graph import *
 from pyArango.theExceptions import *
 
-# we expect the server to be available on localhost.
-conn = Connection(arangoURL = "http://localhost:8529", username="root", password="")
+# Configure your ArangoDB server connection here
+conn = Connection(arango_url="http://localhost:8529", username="USERNAME", password="SECRET")
+
+db = None
+edgeCols = {}
+packagesCol = {}
+
 # we create our own database so we don't interfere with userdata:
-db = {}
-if not conn.hasDatabase("testdb"): 
+
+if not conn.hasDatabase("testdb"):
     db = conn.createDatabase("testdb")
 else:
     db = conn["testdb"]
 
-packagesCol = {}
 if not db.hasCollection('packages'):
     packagesCol = db.createCollection('Collection', name='packages')
-else:    
+else:
     packagesCol = db.collections['packages']
 
-edgeCols = {}
+
 def getEdgeCol(name):
-    global edgeCols, db
     if not name in edgeCols:
         if not db.hasCollection(name):
-            edgeCols[name] = db.createCollection(name=name, className = 'Edges')
+            edgeCols[name] = db.createCollection(name=name, className='Edges')
         else:
-            edgeCols[name] = edgeCol = db.collections[name]
+            edgeCols[name] = db.collections[name]
     return edgeCols[name]
+
+
+def saveGraphDefinition():
+    graph_collection = db.collections["_graphs"]
+    graph_defintion = {
+        "_key": "debian_dependency_graph",
+        "edgeDefinitions": [],
+        "orphanCollections": [],
+    }
+    for collection in edgeCols.keys():
+        graph_defintion["edgeDefinitions"].append(
+            {"collection": collection,
+             "from": ["packages",],
+             "to": ["packages",]})
+    graph_collection.createDocument(graph_defintion).save()
 
 
 def VersionedDependencyToDict(oneDep, hasAlternatives):
@@ -50,11 +80,13 @@ def VersionedDependencyToDict(oneDep, hasAlternatives):
         'hasAlternatives': hasAlternatives
         }
 
+
 def DependencyToDict(oneDep, hasAlternatives):
-        return {
-            'name': oneDep.name,
-            'hasAlternatives': hasAlternatives
-            }
+    return {
+        'name': oneDep.name,
+        'hasAlternatives': hasAlternatives
+        }
+
 
 def DependencySetToDict(dep, hasAlternatives):
     depset = []
@@ -67,30 +99,32 @@ def DependencySetToDict(dep, hasAlternatives):
             depset.append(DependencyToDict(oneDep, hasAlternatives))
         else:
             print("Unknown relationshitp: " + repr(oneDep))
-            
     return depset
+
 
 def PackageToDict(pkg):
     # packages aren't serializable by default, translate it:
     ret = {}
-    for key in pkg.keys():
-        if isinstance(pkg[key], deb_pkg_tools.deps.RelationshipSet):
+    for attribute in pkg.keys():
+        if isinstance(pkg[attribute], deb_pkg_tools.deps.RelationshipSet):
             # relation ship field to become an array of relations:
-            ret[key] = DependencySetToDict(pkg[key], False)
+            ret[attribute] = DependencySetToDict(pkg[attribute], False)
         else:
             # regular string field:
-            ret[key] = pkg[key]
+            ret[attribute] = pkg[attribute]
+    ret["_key"] = ret["Package"]
     return ret
 
+
 def saveDependencyToEdgeCol(edgeCol, dep, pname, hasAlternatives):
-    for oneDep in dep.relationships: 
+    for oneDep in dep.relationships:
         if isinstance(oneDep, deb_pkg_tools.deps.VersionedRelationship):
             # version dependend relations:
             d = VersionedDependencyToDict(oneDep, hasAlternatives)
             d['_from'] = 'packages/' + pname
             d['_to'] = 'packages/' + oneDep.name
             relation = edgeCol.createDocument(d).save()
-        elif isinstance(oneDep, deb_pkg_tools.deps.AlternativeRelationship): 
+        elif isinstance(oneDep, deb_pkg_tools.deps.AlternativeRelationship):
             # A set of alternative relations; recurse:
             saveDependencyToEdgeCol(edgeCol, oneDep, pname, True)
         elif isinstance(oneDep, deb_pkg_tools.deps.Relationship):
@@ -102,6 +136,10 @@ def saveDependencyToEdgeCol(edgeCol, dep, pname, hasAlternatives):
         else:
             print("Unknown relationshitp: " + repr(oneDep))
 
+#
+# Main import routine
+#
+
 onePackage = ''
 for line in open('/tmp/allpackages', encoding='utf-8'):
     # Package blocks are separated by new lines.
@@ -110,13 +148,17 @@ for line in open('/tmp/allpackages', encoding='utf-8'):
         pname = pkg['Package']
         pkg1 = parse_control_fields(pkg)
         p = PackageToDict(pkg1)
-        packagesCol.createDocument(p).save()
-        
-        for key in pkg1.keys():
-            # filter for fields with relations:
-            if isinstance(pkg1[key], deb_pkg_tools.deps.RelationshipSet):
-                # save one relation set to field:
-                saveDependencyToEdgeCol(getEdgeCol(key), pkg1[key], pname, False) 
-        onePackage = ''
+        try:
+            packagesCol.createDocument(p).save()
+            for key in pkg1.keys():
+                # filter for fields with relations:
+                if isinstance(pkg1[key], deb_pkg_tools.deps.RelationshipSet):
+                    # save one relation set to field:
+                    saveDependencyToEdgeCol(getEdgeCol(key), pkg1[key], pname, False)
+            onePackage = ''
+        except CreationError:
+            pass
     else:
         onePackage += line
+
+saveGraphDefinition()
